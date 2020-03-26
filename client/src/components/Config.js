e85afa2e8dc30f331f13b748fc89eb360f250fcb
Ariunbold <ariun1118@gmail.com>
2020-03-26 16:47:31 -0400

    installed deps

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = "5e1bb6654d75f1d312c7e4193c1a534b"
+
+export const API_URL = "https://api.themoviedb.org/3/"
+
+export const IMG_URL = 'https://image.tmdb.org/t/p/'
