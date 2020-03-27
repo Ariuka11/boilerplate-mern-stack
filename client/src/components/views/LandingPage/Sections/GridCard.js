40b06c2c0e1524d17bcf1b58cbbe9988a7f1464a
Ariunbold <ariun1118@gmail.com>
2020-03-27 17:47:27 -0400

    fixed grid size

--- client/src/components/views/LandingPage/Sections/GridCard.js
+++ client/src/components/views/LandingPage/Sections/GridCard.js
@@ -8,7 +8,7 @@ function GridCard(props) {
             <div>
                 <Col key={castId} lg={6} md={8} xs={24}>
                     <div style={{ position: 'relative' }}>
-                        <img style={{ width: '80%', height: '350px', borderRadius: "10px" }} alt={name} src={portrait} />
+                        <img style={{ width: '75%', height: '470px', borderRadius: "10px" }} alt={name} src={portrait} />
                             <h3>{name}</h3>
                     </div>
                 </Col>
@@ -20,7 +20,7 @@ function GridCard(props) {
                 <Col key={movieId} lg={6} md={8} xs={24}>
                     <div style={{ position: 'relative' }}>
                         <a href={`/movie/${movieId}`} >
-                            <img style={{ width: '85%', height: '450px', borderRadius: "10px" }} alt={title} src={image} />
+                            <img style={{ width: '80%', height: '470px', borderRadius: "10px", margin: "2%"}} alt={title} src={image} />
                         </a>
                     </div>
                 </Col>
